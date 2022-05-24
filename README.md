4614f61ca5c747011f1296447f8cff8b942154cb
NateCode <nate@natescode.com>
2022-05-23 21:27:51 -0500

    start of wands and swords rpg

---
+++ README.md
@@ -0,0 +1,20 @@
+# Wands and Words rpg game
+
+Made with Typescript and Canvas API. 
+
+
+## Setup
+
+### Install dependencies
+
+      // install pnpm first
+      pnpm install
+
+### Run webserver
+
+    pnpm start
+
+
+## The Game
+
+Since *Wands & Swords* is a bit of a throwback to Zelda and the NES era, so will the resolution. The nes was 30x28 with 8x8 tiles. We'll keep the same aspect ratio but do 30x28 with 32x32 tiles.
---
+++ wands_and_swords.tsx
@@ -0,0 +1,49 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="wands_and_swords" tilewidth="16" tileheight="16" tilecount="15" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="7">
+  <image width="16" height="16" source="assets/tiles/beach_16x16.png"/>
+ </tile>
+ <tile id="8">
+  <image width="16" height="16" source="assets/tiles/grass_16x16.png"/>
+ </tile>
+ <tile id="9">
+  <image width="16" height="16" source="assets/tiles/lamp_on_table.png"/>
+ </tile>
+ <tile id="10">
+  <image width="16" height="16" source="assets/tiles/night_stand.png"/>
+ </tile>
+ <tile id="11">
+  <image width="16" height="16" source="assets/tiles/water_16x16.png"/>
+ </tile>
+ <tile id="12">
+  <image width="16" height="16" source="assets/tiles/wood_bed.png"/>
+ </tile>
+ <tile id="13">
+  <image width="16" height="16" source="assets/tiles/wood_floor.png"/>
+ </tile>
+ <tile id="14">
+  <image width="16" height="16" source="assets/tiles/sand.png"/>
+ </tile>
+ <tile id="15">
+  <image width="16" height="16" source="assets/tiles/big tree.png"/>
+ </tile>
+ <tile id="16">
+  <image width="16" height="16" source="assets/tiles/big tree.png"/>
+ </tile>
+ <tile id="17">
+  <image width="16" height="16" source="assets/tiles/tree.png"/>
+ </tile>
+ <tile id="18">
+  <image width="16" height="16" source="assets/tiles/dock.png"/>
+ </tile>
+ <tile id="19">
+  <image width="16" height="16" source="assets/tiles/dark_water.png"/>
+ </tile>
+ <tile id="20">
+  <image width="16" height="16" source="assets/tiles/dock_partial.png"/>
+ </tile>
+ <tile id="21">
+  <image width="16" height="16" source="assets/tiles/zalda.png"/>
+ </tile>
+</tileset>
